248c9b4b6350ac389154c04c968121454bdd87ac
V-Conet <46270998+V-Conet@users.noreply.github.com>
2023-01-20 14:27:10 +0800

    icp-moe

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">o-v-i</a>. Made with ❤ by SpencerWoo.',
+    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">o-v-i</a>. <a href="https://icp.gov.moe/?keyword=20230179" target="_blank">萌ICP备20221493号</a>.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
